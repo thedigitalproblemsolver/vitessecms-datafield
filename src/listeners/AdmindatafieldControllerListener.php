bb24edb8ef38d4c8b283043411870e8910e2f91d
Jasper Jaklofsky <info@thedigitalproblemsolver.com>
2021-02-12 19:09:45 +0100

    Moved filterForm to Listeners

---
+++ src/listeners/AdmindatafieldControllerListener.php
@@ -0,0 +1,27 @@
+<?php declare(strict_types=1);
+
+namespace VitesseCms\Datafield\Listeners;
+
+use Phalcon\Events\Event;
+use VitesseCms\Admin\AbstractAdminController;
+use VitesseCms\Admin\Forms\AdminlistFormInterface;
+use VitesseCms\Form\Helpers\ElementHelper;
+use VitesseCms\Form\Models\Attributes;
+
+class AdmindatafieldControllerListener
+{
+    public function adminListFilter(
+        Event $event,
+        AbstractAdminController $controller,
+        AdminlistFormInterface $form
+    ): string
+    {
+        $form->addNameField($form);
+        $form->addPublishedField($form);
+
+        return $form->renderForm(
+            $controller->getLink() . '/' . $controller->router->getActionName(),
+            'adminFilter'
+        );
+    }
+}
